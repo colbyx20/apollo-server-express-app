31d82d57837fc778ec7191c3b923f5ba66c12f70
Colby <colbyx20@gmail.com>
2023-01-14 12:26:41 -0500

    userInfo Schema

--- models/Professors.model.js
+++ models/Professors.model.js
@@ -3,17 +3,8 @@ const{model,Schema} = require('mongoose');
 const professorSchema = new Schema({
     professorFName: {type:String, require:true},
     professorLName: {type:String, require:true},
-    email:{type:String, require:true, unique: true},
     availSchedule: {type: Array},
-    appointments: {type:Array},
-    image:{type:String},
-    coordinator: {type: Boolean},
-    privilege: {
-        type: String,
-        require: true,
-        enum:['user','professor','coordinator'],
-        default:'professor' 
-    }
+    appointments: {type:Array}
 });
 
 module.exports = model('Professor',professorSchema);
