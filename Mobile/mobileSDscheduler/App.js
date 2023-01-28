6899c7f67307ca60f4c6a7267454ffda93d34491
serpenger <matias.libryk@outlook.com>
2023-01-27 20:16:18 -0500

    Added base app

---
+++ Mobile/mobileSDscheduler/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>I got it to work! This is my "Hello World!" equivalent :P</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
