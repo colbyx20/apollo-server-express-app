9de5c7327d6abb6cd45acb37217805b46f977a15
Colby <colbyx20@gmail.com>
2023-02-11 16:39:39 -0500

    update

--- Web/client/src/pages/professors.js
+++ Web/client/src/pages/professors.js
@@ -2,7 +2,7 @@ import {useContext} from 'react';
 
 // When register is created, we needto call AuthProvider function
 import { AuthContext } from '../context/authContext'; 
-import Navbar from '../components/navbar';
+import Sidebar from '../components/Sidebar';
 // import { GetGroups } from '../components/GetGroups';
 // import {GetAvailSchedule} from '../components/GetAvailSchedule';
 
@@ -11,23 +11,13 @@ function Professors(props){
     // user data lives in here 
     const {user} = useContext(AuthContext);
 
+
     return (
         <>
-        <Navbar />
+        <div>
+            <Sidebar />
+        </div>
 
-            <div className="userInfo">
-                <p style={{textAlign:"left", margin:0}}>{user.email}</p>
-                <p style={{textAlign:"left", margin:0}}>{user.firstname}</p>
-                <p style={{textAlign:"left", margin:0}}>{user.lastname}</p>
-                
-                <br />
-
-                {/* <GetGroups /> */}
-
-                <br />
-                {/* <GetAvailSchedule /> */}
-
-              </div>
         </>
 
     )
