89085f57b23e0fb03eb945e8db14249cfcb6c8f1
Andy Garcia <60903978+AndyGarcia102@users.noreply.github.com>
2023-02-15 15:05:42 -0500

    Updated components and links
    Fixed sidebar padding and centering issues.

--- Web/client/src/components/datasidebar.js
+++ Web/client/src/components/datasidebar.js
@@ -4,11 +4,12 @@ import Home from '../components/images/house-door.svg';
 import Building from '../components/images/building.svg';
 import Calendar from '../components/images/calendar3-week.svg';
 import Profile from '../components/images/person-circle.svg'
-import {useNavigate} from 'react-router-dom';
+import Gear from '../components/images/gear.svg';
+import Logout from '../components/images/door-open.svg'
+
 
 const firstInitial = localStorage.getItem("firstname");
 const lastname = localStorage.getItem("lastname");
-
 
 function Formatter(string) {
 
@@ -26,7 +27,7 @@ export const datasidebar = [
     {
         title: <p className='sideBarText'>Home</p>,
         icon: <img className='icon' src={Home}></img>,
-        link: "/home"
+        link: "Homepage"
     },
     {
         title: <p className='sideBarText'>Semester</p>,
@@ -38,4 +39,14 @@ export const datasidebar = [
         icon: <img className='icon' src={Calendar}></img>,
         link: "/calendar"
     },
+    {
+        title: <p className='sideBarText'>Settings</p>,
+        icon: <img className='icon' src={Gear}></img>,
+        link: "/settings"
+    },
+    {
+        title: <p className='sideBarText'>Logout</p>,
+        icon: <img className='icon' src={Logout}></img>,
+        link: "/"
+    }
 ]
