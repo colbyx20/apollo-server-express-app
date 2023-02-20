29d74036366c078c2ccb738ae4b82aa256165984
Zachariah <bjs02365@gmail.com>
2023-02-20 18:49:23 -0500

    revert a bit

--- models/CoordSchedule.model.js
+++ models/CoordSchedule.model.js
@@ -4,6 +4,7 @@ const{model,Schema,mongoose} = require('mongoose');
 const coordScheduleSchema = new Schema({
     coordinatorID:{type:Schema.Types.ObjectId},
     room:{type:String},
+    //groupId clicks better in my head but do to the nature of 
     //groupId:{type: Schema.Types.ObjectId}, scrapping this as i cannot controll the _id of an object and therefore cannot verify that im can pick an unused ID
     groupNumber:{type: Number, min:1}, 
     //p.s. if your going to make a change like this notify me
