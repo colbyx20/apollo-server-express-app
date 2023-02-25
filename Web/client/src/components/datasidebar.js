5a6f4f3f7cce5ffdb1096f8896414305f6ccfc63
Andy Garcia <60903978+AndyGarcia102@users.noreply.github.com>
2023-02-24 20:07:37 -0500

    Big commit
    stuff

--- Web/client/src/components/datasidebar.js
+++ Web/client/src/components/datasidebar.js
@@ -4,19 +4,20 @@ import Home from '../components/images/house-door.svg';
 import Building from '../components/images/building.svg';
 import Calendar from '../components/images/calendar3-week.svg';
 import Profile from '../components/images/person-circle.svg'
-import Gear from '../components/images/gear.svg';
 import Logout from '../components/images/door-open.svg'
 
 
 const firstInitial = localStorage.getItem("firstname");
 const lastname = localStorage.getItem("lastname");
 
+// Check logic if img null then use icon
+// function to return image
 
 export const datasidebar = [
     {
         title: <p className='sideBarText'>Account</p>,
         icon: <img className='iconHeader' src={Profile}></img>,
-        link: "/user"
+        link: "/account"
     },
     {
         title: <p className='sideBarText'>Home</p>,
@@ -32,11 +33,6 @@ export const datasidebar = [
         title: <p className='sideBarText'>Calendar</p>,
         icon: <img className='icon' src={Calendar}></img>,
         link: "/calendar"
-    },
-    {
-        title: <p className='sideBarText'>Settings</p>,
-        icon: <img className='icon' src={Gear}></img>,
-        link: "/settings"
     },
     {
         title: <p className='sideBarText'>Logout</p>,
--- Web/client/src/pages/coordinator.js
+++ Web/client/src/pages/coordinator.js
@@ -1,17 +1,13 @@
 import {useContext, useState} from 'react';
 import { AuthContext } from '../context/authContext'; 
 import CustomSidebar from '../components/Sidebar';
-import { GetGroups } from '../components/GetGroups';
 import { useNavigate } from 'react-router-dom';
 import {Button} from "@mui/material";
 import '../components/css/coordinator.css';
-import FileUpload from '../components/FileUpload';
-
-// import {GetAvailSchedule} from '../components/GetAvailSchedule';
 
 function Coordinator(props){
 
-   // user data lives in here 
+    // user data lives in here 
     const {user, logout} = useContext(AuthContext);
     let navigate = useNavigate();
     var year = new Date().getFullYear()
@@ -22,36 +18,28 @@ function Coordinator(props){
         navigate('/');
     }
 
-    const [searchInput, setSearchInput] = useState("");
-
-    const handleChange = (e) => {
-        e.preventDefault();
-        setSearchInput(e.target.value);
-    };
-
     return (
         <>
-         <div className='coordPage'>
-            {user !== null?
-                <>
-                <CustomSidebar/>
-                <div className='coordWrapper'>
-                    <div className='userInfo'>
-                        <p className='coordHeader'>Home Page</p>
+            <div className='coordPage'>
+                {user !== null?
+                    <>
+                    <CustomSidebar/>
+                    <div className='coordWrapper'>
+                        <div className='userInfo'>
+                            <p className='coordHeader'>Home Page</p>
+                        </div>
+                        
                     </div>
-                    
-                </div>
-                </>
-                : 
-                <>
-                    <div className='noUser'>
-                        <h3>No Page Found</h3>
-                        <Button style={{color:'white'}}onClick={onLogout}>Redirect to Login</Button>
-                    </div>
-                </>
-            }
-        </div>
-
+                    </>
+                    : 
+                    <>
+                        <div className='noUser'>
+                            <h3>No Page Found</h3>
+                            <Button style={{color:'white'}}onClick={onLogout}>Redirect to Login</Button>
+                        </div>
+                    </>
+                }
+            </div>
         </>
 
     )
