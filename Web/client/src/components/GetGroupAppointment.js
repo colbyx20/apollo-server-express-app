c8fc7e5b9c2cd35d88e9e96f742f2ec928a12e9c
Andy Garcia <60903978+AndyGarcia102@users.noreply.github.com>
2023-03-19 00:03:11 -0400

    stylgn
    styling

--- Web/client/src/components/GetGroupAppointment.js
+++ Web/client/src/components/GetGroupAppointment.js
@@ -22,33 +22,19 @@ export const GetGroupAppointment = ({ SID }) => {
     const { loading, error, data } = useQuery(GET_GROUP_APPOINTMENT, {
         variables: { studentId: SID },
     })
-    console.log(data);
 
     if (loading) return 'Loading...';
     if (error) return `Error! ${error.message}`
     const myData = data.getGroupAppointment
-    // console.log(data.getGroupMembers);
-    console.log("My Data");
-    console.log(myData);
     return (
         <table>
             <tbody>
-                <tr key={myData._id}>
-                    <td style={{ border: "2px solid black" }}>Time: {myData.time}</td>
+                <tr >
+                    <td style={{ border: "2px dotted black" }}>Attending Professors: {myData.attending2.map((member) => {
+                        return (<tr key={member._id}>{member.fullName}</tr>)
+                    })}</td>
                 </tr>
-                <tr>
-                    <td style={{ border: "2px solid black" }}>Room: {myData.room}</td>
-                </tr>
-                <table>
-                    <tbody>
-                        <tr >
-                            <td style={{ border: "2px solid black" }}>Attending Professors: {myData.attending2.map((member) => {
-                                return (<tr key={member._id}>{member.fullName}</tr>)
-                            })}</td>
-                        </tr>
-                    </tbody>
-                </table>
             </tbody>
-        </table >
+        </table>
     )
 }
