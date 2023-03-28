75518b95c56a9e6b87711cf53b10ebce44786404
Colby <colbyx20@gmail.com>
2023-03-28 14:00:14 -0400

    clean up localStorage settings

--- Web/client/src/pages/calendar2.js
+++ Web/client/src/pages/calendar2.js
@@ -17,6 +17,8 @@ import "../components/css/calendar2.css"
 function Calendar(props) {
     // user data lives in here  
     const { user, logout } = useContext(AuthContext);
+    console.log("My USER");
+    console.log(user);
     let navigate = useNavigate();
     const requestAbortController = useRef(null);
     const [isLoading, setIsLoading] = useState(false);
@@ -33,12 +35,12 @@ function Calendar(props) {
     const [getScheduleDate, setScheduleDate] = useState([]);
     const [getTimeRange, setTimeRange] = useState([]);
     const timeRangeData = GetCoordinatorTimeRange({ ID: user.id });
-    
-    if(timeRangeData[0]){
+
+    if (timeRangeData[0]) {
         currentDate = new Date(timeRangeData[0].time);
         value = new Date(timeRangeData[0].time);
     }
-    
+
 
     const onLogout = () => {
         logout();
@@ -104,127 +106,127 @@ function Calendar(props) {
         setScheduleDate(item);
     };
 
-    const addTimeList = (item) =>{
+    const addTimeList = (item) => {
         setTimeRange(item);
     }
-    return(
+    return (
         <>
             <div className='calendar2Page'>
-            {user !== null?
-                <>
-                    <CustomSidebar/>
-                    <div className='calendar2Wrapper'>
-                        <div className='userInfo'>
-                            <p className='accountHeader'>Calendar</p>
-                        </div>
-                        <div className='leftContainer'>
-                            <div className='coordWeekContainer'>
-                                <h2 className='calendar-Title'>Set Design Week</h2>
-                                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                    <DatePicker
-                                        label="Start Date"
-                                        minDate={currentDate}
-                                        maxDate={maxDate}
-                                        value={value}
-                                        loading={isLoading}
-                                        onChange={(newValue) => {
-                                            setValue(newValue);
-                                            setSecondPickerEnabled(true);
-                                        }}
-                                        shouldDisableDate={shouldDisableDate}
-                                        renderInput={(params) => <TextField {...params} />}
-                                        renderLoading={() => <CalendarPickerSkeleton />}
-                                        renderDay={(day, _value, DayComponentProps) => {
-                                        const isSelected =
-                                            !DayComponentProps.outsideCurrentMonth &&
-                                            highlightedDays.indexOf(day.date()) > 0;
-                                            
-
-                                        return (
-                                            <Badge>
-                                            <PickersDay {...DayComponentProps} />
-                                            </Badge>
-                                        );
-                                        }}
-                                    />
-                                </LocalizationProvider>
-                                <br/>
-                                <LocalizationProvider
-                                dateAdapter={AdapterDayjs}>
-                                    <DatePicker
-                                        label="End Date"
-                                        minDate={value}   
-                                        maxDate={maxDate}
-                                        loading={isLoading}
-                                        value={endValue}
-                                        onChange={(newValue) => {
-                                            setEndValue(newValue);
-                                            setIsValid(true);
-                                        }}
-                                        shouldDisableDate={shouldDisableDate}
-                                        renderInput={(params) => 
-                                        <TextField {...params} />}
-                                        renderLoading={() => <CalendarPickerSkeleton />}
-                                        renderDay={(day, _value, DayComponentProps) => {
-                                        const isSelected =
-                                            !DayComponentProps.outsideCurrentMonth &&
-                                            highlightedDays.indexOf(day.date()) > 0;
-
-                                        return (
-                                            <Badge>
-                                            <PickersDay {...DayComponentProps} />
-                                            </Badge>
-                                        );
-                                        }}
-                                    />
-                                </LocalizationProvider>
-
-                                <Button variant="contained" color="primary" type="submit"
-                                style={{ width: "236px", margin: "auto", marginTop: '4px', display: "flex", alignItems: "center" }}
-                                disabled={!isValid}
-                                onClick={handleSubmit}
-                                >Submit</Button>
-                                 {isSubmitted && (
-                                    <Typography variant="subtitle1" color="success">
-                                    Dates submitted successfully!
-                                    </Typography>
-                                )}
-
+                {user !== null ?
+                    <>
+                        <CustomSidebar />
+                        <div className='calendar2Wrapper'>
+                            <div className='userInfo'>
+                                <p className='accountHeader'>Calendar</p>
                             </div>
-                            <div className='calendar-container'>
+                            <div className='leftContainer'>
+                                <div className='coordWeekContainer'>
+                                    <h2 className='calendar-Title'>Set Design Week</h2>
+                                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                        <DatePicker
+                                            label="Start Date"
+                                            minDate={currentDate}
+                                            maxDate={maxDate}
+                                            value={value}
+                                            loading={isLoading}
+                                            onChange={(newValue) => {
+                                                setValue(newValue);
+                                                setSecondPickerEnabled(true);
+                                            }}
+                                            shouldDisableDate={shouldDisableDate}
+                                            renderInput={(params) => <TextField {...params} />}
+                                            renderLoading={() => <CalendarPickerSkeleton />}
+                                            renderDay={(day, _value, DayComponentProps) => {
+                                                const isSelected =
+                                                    !DayComponentProps.outsideCurrentMonth &&
+                                                    highlightedDays.indexOf(day.date()) > 0;
+
+
+                                                return (
+                                                    <Badge>
+                                                        <PickersDay {...DayComponentProps} />
+                                                    </Badge>
+                                                );
+                                            }}
+                                        />
+                                    </LocalizationProvider>
+                                    <br />
+                                    <LocalizationProvider
+                                        dateAdapter={AdapterDayjs}>
+                                        <DatePicker
+                                            label="End Date"
+                                            minDate={value}
+                                            maxDate={maxDate}
+                                            loading={isLoading}
+                                            value={endValue}
+                                            onChange={(newValue) => {
+                                                setEndValue(newValue);
+                                                setIsValid(true);
+                                            }}
+                                            shouldDisableDate={shouldDisableDate}
+                                            renderInput={(params) =>
+                                                <TextField {...params} />}
+                                            renderLoading={() => <CalendarPickerSkeleton />}
+                                            renderDay={(day, _value, DayComponentProps) => {
+                                                const isSelected =
+                                                    !DayComponentProps.outsideCurrentMonth &&
+                                                    highlightedDays.indexOf(day.date()) > 0;
+
+                                                return (
+                                                    <Badge>
+                                                        <PickersDay {...DayComponentProps} />
+                                                    </Badge>
+                                                );
+                                            }}
+                                        />
+                                    </LocalizationProvider>
+
+                                    <Button variant="contained" color="primary" type="submit"
+                                        style={{ width: "236px", margin: "auto", marginTop: '4px', display: "flex", alignItems: "center" }}
+                                        disabled={!isValid}
+                                        onClick={handleSubmit}
+                                    >Submit</Button>
+                                    {isSubmitted && (
+                                        <Typography variant="subtitle1" color="success">
+                                            Dates submitted successfully!
+                                        </Typography>
+                                    )}
+
+                                </div>
+                                <div className='calendar-container'>
                                     <h2 className='calendar-Title'>Calendar</h2>
                                     <GlobalCalendar
                                         daysList={selectedWeek}
                                         minDate={currentDate}
                                         maxDate={maxDate} />
                                 </div>
-                        </div>
-                        <div className='rightContainer'>
-                            <div className='selectTimes'>
-                                <h2 className='timeTitle'>Create Schedule</h2>
-                                <div className='timeContainer'>
-                                <DisplayDesignWeek daysList={selectedWeek} isEmpty={isEmpty} 
-                                onScheduleDate={addDateList} onTimeRange={addTimeList}/>
-                                </div>
-                                <h2 className='timeTitle'>View Schedule</h2>
-                                <div className='viewSchedule'>
-                                    <DisplaySchedule pickList={getScheduleDate} timeList={getTimeRange} 
-                                    dateList={selectedWeek}/>
+                            </div>
+                            <div className='rightContainer'>
+                                <div className='selectTimes'>
+                                    <h2 className='timeTitle'>Create Schedule</h2>
+                                    <div className='timeContainer'>
+                                        <DisplayDesignWeek daysList={selectedWeek} isEmpty={isEmpty}
+                                            onScheduleDate={addDateList} onTimeRange={addTimeList} />
+                                    </div>
+                                    <h2 className='timeTitle'>View Schedule</h2>
+                                    <div className='viewSchedule'>
+                                        <DisplaySchedule pickList={getScheduleDate} timeList={getTimeRange}
+                                            dateList={selectedWeek} />
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                                
-                        
-                </>
-                :
-                <>
-                    <div className='noUser'>
-                        <h3>No Page Found</h3>
-                        <Button style={{ color: 'white' }} onClick={onLogout}>Redirect to Login</Button>
-                    </div>
-                </>
-            }
+
+
+                    </>
+                    :
+                    <>
+                        <div className='noUser'>
+                            <h3>No Page Found</h3>
+                            <Button style={{ color: 'white' }} onClick={onLogout}>Redirect to Login</Button>
+                        </div>
+                    </>
+                }
             </div>
         </>
     )
