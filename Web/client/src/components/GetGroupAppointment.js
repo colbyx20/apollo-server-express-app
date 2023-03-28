580f799ffebe770c4bece87f074aae5b0f262130
Colby <colbyx20@gmail.com>
2023-03-27 20:43:26 -0400

    CSS styling and logic to Design meeting on users

--- Web/client/src/components/GetGroupAppointment.js
+++ Web/client/src/components/GetGroupAppointment.js
@@ -27,27 +27,35 @@ export const GetGroupAppointment = ({ SID }) => {
     if (error) return `Error! ${error.message}`
     const myData = data.getGroupAppointment
 
+    console.log(myData);
+
     return (
-        <table>
-            <tbody>
-                <tr >
-                    <div className='getappInfo'>
-                        {
-                        new Date(myData.time).toLocaleDateString('en-us',{month: 'numeric'})+"/"+
-                        new Date(myData.time).toLocaleDateString('en-us',{day: 'numeric'})+"/"+
-                        new Date(myData.time).toLocaleDateString('en-us',{year: 'numeric'})
-                        }<br/>
-                        {myData.room}<br/>
-                        {new Date(myData.time).toLocaleTimeString('en-US', { timeZone: 'UTC', hour: 'numeric', minute: 'numeric', hour12: true })}
-                        
-                        
-                        
-                    </div>
-                    <td>Attending Professors: {myData?.attending2.map((member) => {
-                        return (<tr key={member._id}>{member.fullName}</tr>)
-                    })}</td>
-                </tr>
-            </tbody>
-        </table>
+        <>
+            {myData === null ?
+                <span className='noAppointmentMessage'>Appointment has not been Set</span>
+                :
+                <table>
+                    <tbody>
+                        <tr >
+                            <div className='getappInfo'>
+                                {
+                                    new Date(myData?.time).toLocaleDateString('en-us', { month: 'numeric' }) + "/" +
+                                    new Date(myData?.time).toLocaleDateString('en-us', { day: 'numeric' }) + "/" +
+                                    new Date(myData?.time).toLocaleDateString('en-us', { year: 'numeric' })
+                                }<br />
+                                {myData?.room}<br />
+                                {new Date(myData?.time).toLocaleTimeString('en-US', { timeZone: 'UTC', hour: 'numeric', minute: 'numeric', hour12: true })}
+
+                            </div>
+                            <td>Attending Professors: {myData?.attending2.map((member) => {
+                                return (<tr key={member?._id}>{member?.fullName}</tr>)
+                            })}</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+
+            }
+        </>
     )
 }
