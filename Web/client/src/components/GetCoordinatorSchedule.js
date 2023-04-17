8e69c84008bf8e8e1cc1faa8ac875c71ed08adb6
Colby berger <colbyx20@gmail.com>
2023-04-17 10:19:48 -0400

    add spinning wheel to generating buttons

--- Web/client/src/components/GetCoordinatorSchedule.js
+++ Web/client/src/components/GetCoordinatorSchedule.js
@@ -3,6 +3,8 @@ import { AuthContext } from '../context/authContext';
 import * as React from 'react';
 import { useState, useEffect, useContext } from 'react';
 import Table from '@mui/material/Table';
+import CircularProgress from '@mui/material/CircularProgress';
+import Backdrop from '@mui/material/Backdrop';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
@@ -52,15 +54,16 @@ export const GetCoordinatorSchedule = ({ ID }) => {
     const { user } = useContext(AuthContext);
 
     const [schedule, setSchedule] = useState([]);
-    const [randomlySelectProfessorsToAGroup] = useMutation(GENERATE_APPOINTMENT);
-    const [generateGroupAppointment] = useMutation(GENERATE_GROUP_APPOINTMENTS)
+    const [randomlySelectProfessorsToAGroup, { loading: generateGroupLoading }] = useMutation(GENERATE_APPOINTMENT);
+    const [generateGroupAppointment, { loading: generateCommitteeLoading }] = useMutation(GENERATE_GROUP_APPOINTMENTS)
     const [isHeld, setIsHeld] = useState(false);
+    const [open, setIsOpen] = useState(false);
     const [holdTimeout, setHoldTimeout] = useState(null);
     const [cancel, setCancel] = useState("HOLD TO CANCEL");
     const holdTime = 2800;
 
 
-    const { loading, error, data, networkStatus } = useQuery(GET_SCHEDULE, {
+    const { error, data, networkStatus } = useQuery(GET_SCHEDULE, {
         variables: { cid: ID },
     });
 
@@ -99,22 +102,28 @@ export const GetCoordinatorSchedule = ({ ID }) => {
     };
 
     if (networkStatus === NetworkStatus.refetch) return 'Refetching!';
-    if (loading) return 'Loading...';
+    // if (loading) return 'Loading...';
     if (error) return `Error! ${error.message}`
 
 
     function handleCreateGenerateViewers(e, ID, firstname, lastname) {
+        setIsOpen(true);
         randomlySelectProfessorsToAGroup({
             variables: { cid: ID, fullName: firstname + ' ' + lastname },
-            refetchQueries: [{ query: GET_SCHEDULE, variables: { cid: ID } }]
+            refetchQueries: [{ query: GET_SCHEDULE, variables: { cid: ID } }],
+            onCompleted: setIsOpen(false)
         })
+        // setIsOpen(false);
     }
 
     function handleAssignGroups(e, ID) {
+        setIsOpen(true);
         generateGroupAppointment({
             variables: { cid: ID },
-            refetchQueries: [{ query: GET_SCHEDULE, variables: { cid: ID } }]
+            refetchQueries: [{ query: GET_SCHEDULE, variables: { cid: ID } }],
+            onCompleted: setIsOpen(false)
         })
+        // setIsOpen(false);
     }
 
     function returnCurrentDateTime(date1) {
@@ -126,61 +135,80 @@ export const GetCoordinatorSchedule = ({ ID }) => {
     }
 
     return (
-        <>
-            <div className='StickyHeader'>
-                <Button sx={{ float: 'right', color: 'white', bgcolor: '#1976d2' }} variant='Contained' onClick={(e) => handleCreateGenerateViewers(e, ID, user.firstname, user.lastname)}>Generate Committee</Button>
-                <Button sx={{ float: 'left', color: 'white', bgcolor: '#1976d2' }} variant='Contained' onClick={(e) => handleAssignGroups(e, ID)}>Generate Group</Button>
-            </div>
-            <TableContainer component={Paper} sx={{ bgcolor: '#231F20', height: '95%', overflow: 'none' }}>
-                <Table>
-                    <TableBody>
-                        {schedule.map((coordinator) => {
-                            return (<TableRow
-                                key={coordinator._id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 }, color: 'white' }}
-                            >
-                                <TableCell sx={{ color: 'white' }} align='center'>
-                                    {'Time:'}
-                                    <br />
-                                    {new Date(coordinator.time).toLocaleDateString('en-US', { month: 'long' })}{' '}
-                                    {new Date(coordinator.time).getDate().toLocaleString('en-US', { minimumIntegerDigits: 2 })}
-                                    {new Date(coordinator.time).getDate() % 10 === 1 ? 'st' : new Date(coordinator.time).getDate() % 10 === 2 ? 'nd' : new Date(coordinator.time).getDate() % 10 === 3 ? 'rd' : 'th'},{' '}
-                                    {/* {new Date(coordinator.time).toLocaleTimeString('en-US', {  timeZone: 'America/New_York',hour: 'numeric', minute: 'numeric', hour12: true })} */}
-                                    {returnCurrentDateTime(coordinator.time)}
-                                </TableCell>
-                                <TableCell sx={{ color: 'white' }} align='left'>{'Room:'}<br />{coordinator.room} </TableCell>
-                                <TableCell sx={{ color: 'white' }} align='left'>{'Project:'}<br />{coordinator.groupId?.groupName}</TableCell>
-                                <TableCell sx={{ color: 'white' }} align='left'>{'Attending:'}<br />{coordinator.attending2?.map((e) => {
-                                    return <span sx={{ color: 'white' }} align='right' key={e._id}>{e?.fullName}<br /></span>
-                                })}</TableCell>
-                                <TableCell>
-                                    <Button
-                                        sx={{
-                                            backgroundColor: 'red',
-                                            ':hover': {
-                                                bgcolor: '#8B0000', // On hover
-                                                color: 'white',
-                                            },
-                                            ':active': {
-                                                background: 'black',
-                                                transitionDuration: '3s'
+        <>{(generateCommitteeLoading || generateGroupLoading) ?
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1000 }}
+                open={true}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
 
-                                            }
-                                        }}
-                                        onMouseDown={() => handleMouseDown(coordinator._id)}
-                                        onMouseUp={handleMouseUp}
-                                        onTouchStart={() => handleMouseDown(coordinator._id)}
-                                        onTouchEnd={handleMouseUp}
-                                        variant="contained">
-                                        {cancel}
-                                    </Button>
-                                </TableCell>
-                            </TableRow>
-                            )
-                        })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+            :
+            <>
+                <div className='StickyHeader'>
+                    <Button
+                        sx={{ float: 'right', color: 'white', bgcolor: '#1976d2' }}
+                        variant='Contained'
+                        onClick={(e) => handleCreateGenerateViewers(e, ID, user.firstname, user.lastname)}
+                        disable={generateCommitteeLoading}>Generate Committee</Button>
+                    <Button
+                        sx={{ float: 'left', color: 'white', bgcolor: '#1976d2' }}
+                        variant='Contained'
+                        onClick={(e) => handleAssignGroups(e, ID)}
+                        disable={generateGroupLoading}>Generate Group</Button>
+                </div>
+                <TableContainer component={Paper} sx={{ bgcolor: '#231F20', height: '95%', overflow: 'none' }}>
+                    <Table>
+                        <TableBody>
+                            {schedule.map((coordinator) => {
+                                return (<TableRow
+                                    key={coordinator._id}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 }, color: 'white' }}
+                                >
+                                    <TableCell sx={{ color: 'white' }} align='center'>
+                                        {'Time:'}
+                                        <br />
+                                        {new Date(coordinator.time).toLocaleDateString('en-US', { month: 'long' })}{' '}
+                                        {new Date(coordinator.time).getDate().toLocaleString('en-US', { minimumIntegerDigits: 2 })}
+                                        {new Date(coordinator.time).getDate() % 10 === 1 ? 'st' : new Date(coordinator.time).getDate() % 10 === 2 ? 'nd' : new Date(coordinator.time).getDate() % 10 === 3 ? 'rd' : 'th'},{' '}
+                                        {/* {new Date(coordinator.time).toLocaleTimeString('en-US', {  timeZone: 'America/New_York',hour: 'numeric', minute: 'numeric', hour12: true })} */}
+                                        {returnCurrentDateTime(coordinator.time)}
+                                    </TableCell>
+                                    <TableCell sx={{ color: 'white' }} align='left'>{'Room:'}<br />{coordinator.room} </TableCell>
+                                    <TableCell sx={{ color: 'white' }} align='left'>{'Project:'}<br />{coordinator.groupId?.groupName}</TableCell>
+                                    <TableCell sx={{ color: 'white' }} align='left'>{'Attending:'}<br />{coordinator.attending2?.map((e) => {
+                                        return <span sx={{ color: 'white' }} align='right' key={e._id}>{e?.fullName}<br /></span>
+                                    })}</TableCell>
+                                    <TableCell>
+                                        <Button
+                                            sx={{
+                                                backgroundColor: 'red',
+                                                ':hover': {
+                                                    bgcolor: '#8B0000', // On hover
+                                                    color: 'white',
+                                                },
+                                                ':active': {
+                                                    background: 'black',
+                                                    transitionDuration: '3s'
+
+                                                }
+                                            }}
+                                            onMouseDown={() => handleMouseDown(coordinator._id)}
+                                            onMouseUp={handleMouseUp}
+                                            onTouchStart={() => handleMouseDown(coordinator._id)}
+                                            onTouchEnd={handleMouseUp}
+                                            variant="contained">
+                                            {cancel}
+                                        </Button>
+                                    </TableCell>
+                                </TableRow>
+                                )
+                            })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </>
+        }
         </>
     )
 }
