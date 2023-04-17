d9fcce4d13a6c72adad485b9266cf8e763476cbb
Colby berger <colbyx20@gmail.com>
2023-04-17 13:30:32 -0400

    revert Randomize API to original working order

--- resolver.js
+++ resolver.js
@@ -1206,37 +1206,23 @@ const resolvers = {
 
                     const date = new Date(coordinatorInfo[0].time);
 
-                    // prioritize professors that match and that have not been assigned yet.
-                    const collectProfessors = await Professors.aggregate([
-                        { $match: { availSchedule: date }, appointment: { $size: 0 } },
-                        { $sample: { size: MAX_APPOINTMENTS - coordinatorInfo[0].numberOfAttending } }
+                    const matchProfessors = await Professors.aggregate([
+                        { $match: { availSchedule: date } },
+                        { $sample: { size: MAX_APPOINTMENTS - coordinatorInfo[0].numberOfAttending } },
+                        { $project: { _id: 1, fullName: { $concat: ['$professorFName', ' ', '$professorLName'] } } }
                     ])
 
-                    if (collectProfessors) {
-
-                        const professorInfo = collectProfessors.map((professor) => ({
+                    if (matchProfessors) {
+                        const professorInfo = matchProfessors.map((professor) => ({
                             _id: professor._id,
                             fullName: professor.fullName
                         }));
 
                         await Promise.all([
-                            CoordSchedule.findOneAndUpdate({ coordinatorID: coordinatorId, time: date }, { $inc: { numberOfAttending: collectProfessors.length }, $push: { attending2: { $each: professorInfo } } }),
-                            Professors.updateMany({ _id: { $in: professorInfo } }, { $pull: { availSchedule: date }, $push: { appointments: coordinatorInfo._id } })
-                        ]);
-
-                    } else {
-                        const matchProfessors = await Professors.aggregate([
-                            { $match: { availSchedule: date } },
-                            { $sample: { size: MAX_APPOINTMENTS - coordinatorInfo[0].numberOfAttending } },
-                            { $project: { _id: 1, fullName: { $concat: ['$professorFName', ' ', '$professorLName'] } } }
-                        ])
-                        await Promise.all([
                             CoordSchedule.findOneAndUpdate({ coordinatorID: coordinatorId, time: date }, { $inc: { numberOfAttending: matchProfessors.length }, $push: { attending2: { $each: professorInfo } } }),
                             Professors.updateMany({ _id: { $in: professorInfo } }, { $pull: { availSchedule: date }, $push: { appointments: coordinatorInfo._id } })
                         ]);
                     }
-
-
                 }
             } catch (e) {
                 return false;
